8dde98b124536ca4ee028f52a4d3a53aeb3c4059
three <448987046@qq.com>
2015-10-24 14:01:24 +0800

    support firefox

--- index.js
+++ index.js
@@ -7,14 +7,14 @@ var plantuml = require('./plantuml');
 
 hexo.extend.tag.register('plantuml', function(args, content){
     return new Promise(function (resolve, reject) {
-        var plantSvgUrl = plantuml.compress(content);
-        request(plantSvgUrl, function (error, response, body) {
+        var plantumlSvgUrl = plantuml.compress(content);
+        request(plantumlSvgUrl, function (error, response, body) {
             if (!error && response.statusCode == 200) {
                 resolve(body)
             }
         })
     }).then(function (data) {
-            return '<img src="data:image/svg+xml;utf8,'+data.replace(/"/ig,"'")+'">';
+            return '<img src="data:image/svg+xml;utf8,'+encodeURIComponent(data)+'">';
         });
 
 },{ends: true, async:true});
