f92bcf75601c86c1aefcf0dd3996484b46698a9a
three <448987046@qq.com>
2015-12-02 15:02:00 +0800

    加入hexo配置参数，支持static和dynamic两种方式

--- README.md
+++ README.md
@@ -17,8 +17,15 @@ And add this plugin in your ``_config.yml``.
 
 ## Usage
 
-Specify the code filetype, and whether to use line number in your markdown source file, in
-the common format of tag plugins:
+_config.yml add config:
+
+	tag_plantuml:
+		type: static
+
+**static**: download svg image into html file, "data:image/svg+xml;utf8";
+**dynamic**: use http://www.plantuml.com/plantuml/svg dynamic produce
+
+Specify the code filetype, and whether to use line number in your markdown source file, in the common format of tag plugins:
 
 	{% plantuml %}
         Bob->Alice : hello
--- index.js
+++ index.js
@@ -3,18 +3,28 @@
 var request = require('request');
 var Promise = require('bluebird');
 var plantuml = require('./plantuml');
+var assign = require('object-assign');
 
+hexo.config.tag_plantuml = assign({
+    type:'static', // static | dynamic
+    format: 'svg' // svn | png
+}, hexo.config.tag_plantuml);
 
 hexo.extend.tag.register('plantuml', function(args, content){
     return new Promise(function (resolve, reject) {
         var plantumlSvgUrl = plantuml.compress(content);
-        request(plantumlSvgUrl, function (error, response, body) {
-            if (!error && response.statusCode == 200) {
-                resolve(body)
-            }
-        })
+        if(hexo.config.tag_plantuml.type==='static') {
+            request(plantumlSvgUrl, function (error, response, body) {
+                if (!error && response.statusCode == 200) {
+                    resolve('<img src="data:image/svg+xml;utf8,'+encodeURIComponent(body)+'">');
+
+                }
+            });
+        } else {
+            resolve('<img src="'+plantumlSvgUrl+'">');
+        }
     }).then(function (data) {
-            return '<img src="data:image/svg+xml;utf8,'+encodeURIComponent(data)+'">';
+            return data;
         });
 
 },{ends: true, async:true});
